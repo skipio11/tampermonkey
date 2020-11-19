7fd2a1c9e6f217ae8eaafed9041f2aeb3129f495
Taejong <tjseo11@gmail.com>
2020-11-19 16:49:32 +0900

    Add include for general guide

--- aws-doc-eng-tooltip-appender/aws-doc-eng-tooltip-appender.user.js
+++ aws-doc-eng-tooltip-appender/aws-doc-eng-tooltip-appender.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AWS Document English Tooltip Appender
 // @namespace    https://github.com/skipio11/tampermonkey/
-// @version      0.21
+// @version      0.22
 // @updateURL    https://raw.githubusercontent.com/skipio11/tampermonkey/master/aws-doc-eng-tooltip-appender/aws-doc-eng-tooltip-appender.meta.js
 // @downloadURL  https://raw.githubusercontent.com/skipio11/tampermonkey/master/aws-doc-eng-tooltip-appender/aws-doc-eng-tooltip-appender.user.js
 // @description  This script has a function to display English tooltips in the AWS Korean Document.
