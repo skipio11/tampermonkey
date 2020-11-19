3301dd75f0f9622b0ca60c4ea49c649eb2a8ad58
Taejong <tjseo11@gmail.com>
2020-11-19 14:11:05 +0900

    Change for version update test

--- aws-doc-eng-tooltip-appender/aws-doc-eng-tooltip-appender.meta.js
+++ aws-doc-eng-tooltip-appender/aws-doc-eng-tooltip-appender.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AWS Document English Tooltip Appender
 // @namespace    https://github.com/skipio11/tampermonkey/
-// @version      0.2
+// @version      0.21
 // @updateURL    https://raw.githubusercontent.com/skipio11/tampermonkey/master/aws-doc-eng-tooltip-appender/aws-doc-eng-tooltip-appender.meta.js
 // @downloadURL  https://raw.githubusercontent.com/skipio11/tampermonkey/master/aws-doc-eng-tooltip-appender/aws-doc-eng-tooltip-appender.user.js
 // ==/UserScript==
